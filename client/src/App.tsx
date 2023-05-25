6789fa10794baeec91678535c2062d9762443657
caseyjramirez <caseyjramirez5@gmail.com>
2023-05-25 14:46:31 -0500

    Homepage created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Home from './pages/home';
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div className="App dark">
-      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
