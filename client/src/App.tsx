0dc346efeba30f931424b5d52152f0e92d89d8ab
caseyjramirez <caseyjramirez5@gmail.com>
2023-05-25 14:32:25 -0500

    created scss file to handle light / dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,9 @@ import React from 'react';
 
 function App() {
 
-  const test = process.env.TESTING;
-  console.log(test);
-
 
   return (
-    <div className="App">
+    <div className="App dark">
       <h1>Hello</h1>
     </div>
   );
